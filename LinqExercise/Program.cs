9f9835fb0415f950ffdd25ea8338e0b13c9f15b1
Nick Carlson <CarlsonNicholas82@gmail.com>
2022-12-20 13:00:08 -0500

    Complete

---
+++ LinqExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿//Creating a Video Game List
+
+List<string> games = new List<string>();
+
+//Adding Games to the List. 
+
+games.Add("Pokemon Red");
+games.Add("Super Mario Oddysey");
+games.Add("The Legend of Zelda");
+games.Add("Star Fox 64");
+games.Add("Dragon Quest");
+
+var gameLength = games.OrderBy(x => x.Length);
+
+foreach (var game in gameLength)
+{
+    Console.WriteLine(game);
+}
